7ecb5f8339e505acab302a9317636ac9bca61e84
Julian Andersen <julian.cesar.andersen@gmail.com>
2024-12-27 14:09:25 +0100

    Separate dropdowns from Carousels

---
+++ dropdown.js
@@ -0,0 +1,34 @@
+function initializeDropdowns(selector = '.dropdown') {
+    const dropDowns = document.querySelectorAll(selector);
+    dropDowns.forEach((dropdown) => {
+        const content = dropdown.querySelector('.dropdown-content');
+
+        if (dropdown.classList.contains('clickable')) {
+            dropdown.addEventListener('click', (e) => {
+                e.stopPropagation();
+                content.classList.toggle('visible');
+            })
+        } else if (dropdown.classList.contains('hoverable')) {
+            dropdown.addEventListener('mouseenter', () => {
+                content.classList.toggle('visible');
+            })
+            dropdown.addEventListener('mouseleave', () => {
+                content.classList.remove('visible');
+            })
+        }
+    });
+
+    window.addEventListener('click', (e) => {
+        if (!e.target.matches('.dropbtn')) {
+            const dropdowns = document.getElementsByClassName('dropdown-content');
+            for (let i = 0; i < dropdowns.length; i++) {
+                const openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('visible')) {
+                    openDropdown.classList.remove('visible');
+                }
+            }
+        }
+    });
+};
+
+module.exports = initializeDropdowns;
